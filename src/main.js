4bb485cb4da4480011a248234171a8053d878707
Mark Townsend <mark@ecolizard.com>
2024-01-03 16:03:58 +0000

    Added vue 2 router

--- src/main.js
+++ src/main.js
@@ -3,13 +3,14 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import i18n from './components/Locales'
 import App from './App.vue';
-
+import router from './router'; // Import the router file
 
 Vue.config.productionTip = false
 Vue.use(ElementUI);
 
 new Vue({
   //el: '#app',
+  router, // Add the router instance to the Vue instance
   render: h => h(App),
   i18n
 }).$mount('#app')
---
+++ src/router.js
@@ -0,0 +1,22 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('./App.vue') // Example: Create a Home.vue component
+  },
+  {
+    path: '/about',
+    component: () => import('./App.vue') // Example: Create an About.vue component
+  }
+  // Add more routes as needed
+];
+
+const router = new VueRouter({
+  routes
+});
+
+export default router;
